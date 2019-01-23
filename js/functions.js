0291ccf360a5603975d1693933a78cb87145470f
Daniel <danielborg291@gmail.com>
2019-01-22 19:27:12 -0800

    finalized html and adding js function

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+function addition() {
+    var factor1 = Number(document.getElementById("factor1").value);
+    var factor2 = Number(document.getElementById("factor2").value);
+    var factor3 = Number(document.getElementById("factor3").value);
+
+    document.getElementById("factor3").value = (factor1 + factor2);
+
+
+
+}
