93735dd1cda76a18231181a751e59b8bbcd59f97
Daniel <danielborg291@gmail.com>
2019-01-28 17:22:31 -0800

    created functions tests

---
+++ functions.test.js
@@ -0,0 +1,14 @@
+const sum = require('./functions');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(addition(1, 2)).toBe(3);
+});
+
+test('adds 30 + 70 to equal 100', () => {
+    expect(addition(30, 70)).toBe(100);
+  });
+
+  //fails on purpose
+  test('adds 20 + 70 to equal 100', () => {
+    expect(addition(20, 70)).toBe(100);
+  });
